523289a9b994063f92f23c3c73ca2d017dd7a345
Dr. Johann Pfefferl <johann.pfefferl@siemens.com>
2015-03-19 11:31:13 +0100

    ppp: helper function separated in own files
    Signed-off-by: Dr. Johann Pfefferl <johann.pfefferl@siemens.com>

--- freertos-runtime/serial.c
+++ freertos-runtime/serial.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include "FreeRTOS.h"
 #include "serial.h"
 
 #ifndef CONFIG_MACH_SUN7I
@@ -119,23 +120,3 @@ int serial_irq_getchar(sio_fd_t fd)
   }
   return r;
 }
-
-/* functions for PPP interface */
-
-static int do_a_read_abort = 0;
-
-u32_t sio_read(sio_fd_t fd, u8_t *data, u32_t len)
-{
-  return -1; // FIXME
-}
-
-u32_t sio_write(sio_fd_t fd, u8_t *data, u32_t len)
-{
-  return len; // FIXME
-}
-
-void sio_read_abort(sio_fd_t fd)
-{
-  do_a_read_abort = 1;
-}
-
---
+++ sio_ppp.h
@@ -0,0 +1,9 @@
+#ifndef _SIO_PPP_C_
+#define _SIO_PPP_C_
+#include "arch/cc.h"
+#include "serial.h"
+/* functions for PPP interface */
+u32_t sio_read(sio_fd_t fd, u8_t *data, u32_t len);
+u32_t sio_write(sio_fd_t fd, u8_t *data, u32_t len);
+void sio_read_abort(sio_fd_t fd);
+#endif
