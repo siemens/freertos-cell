5738b4a93054c5a635308f4f2a6a771df39f2106
Dr. Johann Pfefferl <johann.pfefferl@siemens.com>
2015-03-30 13:57:05 +0200

    Switch off TCP nagle algorithm
    For interactive tcp connection this is really relevant.
    
    Signed-off-by: Dr. Johann Pfefferl <johann.pfefferl@siemens.com>

--- main.c
+++ main.c
@@ -797,8 +797,7 @@ static void echoTcpTask(void *pvParameters)
       // Do not block endless in the receive function
       //netconn_set_recvtimeout(newconn, 2000);
       // Switch off the nagle algorithm
-      //tcp_nagle_disable(newconn->pcb.tcp);
-      /* Now we enter the receiver loop */
+      tcp_nagle_disable(newconn->pcb.tcp);
       while(1) {
         int lcnt = 0;
         /* Is data available on the receive queue */
@@ -823,7 +822,7 @@ static void echoTcpTask(void *pvParameters)
           u16_t len;
           netbuf_data(buf, &data, &len);
           printf("DATA%d: %p l=%u %u\n\r", ++lcnt, data, (unsigned)len, xTaskGetTickCount());
-          err = netconn_write(newconn, data, len, NETCONN_COPY | NETCONN_MORE);
+          err = netconn_write(newconn, data, len, NETCONN_COPY);
           if(ERR_OK != err)
             UART_OUTPUT("%s WARNING: sendto err=%d\n", __func__, err);
         } while(netbuf_next(buf) >= 0);
