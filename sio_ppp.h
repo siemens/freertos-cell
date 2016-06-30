8cf8632f574c4a506e1b6f9d8b758cc590fba93c
Dr. Johann Pfefferl <johann.pfefferl@siemens.com>
2016-06-30 12:23:00 +0200

    Missing function prototype added
    Signed-off-by: Dr. Johann Pfefferl <johann.pfefferl@siemens.com>

--- sio_ppp.h
+++ sio_ppp.h
@@ -80,6 +80,7 @@
 /* functions for PPP interface */
 void sio_timeout_set(sio_fd_t fd, unsigned millisec);
 u32_t sio_read(sio_fd_t fd, u8_t *data, u32_t len);
+u32_t sio_tryread(sio_fd_t fd, u8_t *data, u32_t len);
 u32_t sio_write(sio_fd_t fd, u8_t *data, u32_t len);
 void sio_queue_register(sio_fd_t fd, QueueHandle_t qh);
 void sio_send(u8_t c, sio_fd_t fd);
