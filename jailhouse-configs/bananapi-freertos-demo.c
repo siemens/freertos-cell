e96ba04e456553befeece6a3a560d9838acce0a3
Dr. Johann Pfefferl <johann.pfefferl@siemens.com>
2018-04-09 12:36:29 +0200

    Update for jailhouse config files added
    Signed-off-by: Dr. Johann Pfefferl <johann.pfefferl@siemens.com>

--- jailhouse-configs/bananapi-freertos-demo.c
+++ jailhouse-configs/bananapi-freertos-demo.c
@@ -1,27 +1,27 @@
-#include <linux/types.h>
+#include <jailhouse/types.h>
 #include <jailhouse/cell-config.h>
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 #define MBYTE(m) ((m)<<20)
 
-#define GUEST_MEM_START 0x7a800000
+#define GUEST_MEM_START 0x7a800000 /* XXX */
 #define GUEST_MEM_SIZE MBYTE(16)
 
 struct {
 	struct jailhouse_cell_desc cell;
 	__u64 cpus[1];
-	struct jailhouse_memory mem_regions[3];
-  struct jailhouse_irqchip irqchips[1];
+	struct jailhouse_memory mem_regions[4];
+	struct jailhouse_irqchip irqchips[1];
 } __attribute__((packed)) config = {
 	.cell = {
 		.signature = JAILHOUSE_CELL_DESC_SIGNATURE,
+		.revision = JAILHOUSE_CONFIG_REVISION,
 		.name = "FreeRTOS",
 		.flags = JAILHOUSE_CELL_PASSIVE_COMMREG,
 
 		.cpu_set_size = sizeof(config.cpus),
 		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
-		.num_irqchips = 1,
-		.pio_bitmap_size = 0,
+		.num_irqchips = ARRAY_SIZE(config.irqchips),
 		.num_pci_devices = 0,
 	},
 
@@ -31,7 +31,7 @@ struct {
 
 	.mem_regions = {
 		/* RAM */ {
-      .phys_start = GUEST_MEM_START,
+			.phys_start = GUEST_MEM_START,
 			.virt_start = 0,
 			.size = GUEST_MEM_SIZE,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
@@ -46,15 +46,27 @@ struct {
 		/* CCU, Ints, GPIO, Timer */ {
 			.phys_start = 0x01c20000,
 			.virt_start = 0x01c20000,
-			.size = 0x1000,
-			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE | JAILHOUSE_MEM_IO,
+			.size = 0x400,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE | 
+				JAILHOUSE_MEM_IO | JAILHOUSE_MEM_IO_32 | JAILHOUSE_MEM_ROOTSHARED,
+		},
+#define PIO_P7_DAT_REG (0x01c20800 + 7*0x24 + 0x10)
+		/* PIO port 7: blinking LED */ {
+			.phys_start = PIO_P7_DAT_REG,
+			.virt_start = PIO_P7_DAT_REG,
+			.size = 0x4,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO | JAILHOUSE_MEM_IO_32,
 		},
 	},
-  .irqchips = {
-    /* GIC */ {
-      .address = 0x01c80000,
-      /* Interrupt of UART 7 belongs to the client */
-      .pin_bitmap = 1ULL<<(52-32),
-    },
-  },
+	.irqchips = {
+		/* GIC */ {
+			.address = 0x01c81000, /* XXX perhaps 0x01c81000 */
+			.pin_base = 32,
+			/* Interrupt of UART 7 belongs to the client */
+			.pin_bitmap = {
+				1<<(52-32), 0, 0, 0,
+			},
+		},
+	},
 };
--- jailhouse-configs/bananapi.c
+++ jailhouse-configs/bananapi.c
@@ -12,7 +12,7 @@
  * the COPYING file in the top-level directory.
  */
 
-#include <linux/types.h>
+#include <jailhouse/types.h>
 #include <jailhouse/cell-config.h>
 
 #define ARRAY_SIZE(a) sizeof(a) / sizeof(a[0])
@@ -20,26 +20,50 @@
 struct {
 	struct jailhouse_system header;
 	__u64 cpus[1];
-	struct jailhouse_memory mem_regions[17];
+	struct jailhouse_memory mem_regions[20];
 	struct jailhouse_irqchip irqchips[1];
+	struct jailhouse_pci_device pci_devices[1];
 } __attribute__((packed)) config = {
 	.header = {
 		.signature = JAILHOUSE_SYSTEM_SIGNATURE,
+		.revision = JAILHOUSE_CONFIG_REVISION,
 		.hypervisor_memory = {
 			.phys_start = 0x7c000000,
 			.size = 0x4000000,
 		},
 		.debug_console = {
-			.phys_start = 0x01c28000,
+			.address = 0x01c28000,
 			.size = 0x1000,
-			.flags = JAILHOUSE_MEM_IO,
+			/* .clock_reg = 0x01c2006c, */
+			/* .gate_nr = 16 */
+			/* .divider = 0x0d, */
+			.flags = JAILHOUSE_CON1_TYPE_8250 |
+				 JAILHOUSE_CON1_ACCESS_MMIO |
+				 JAILHOUSE_CON1_REGDIST_4 |
+				 JAILHOUSE_CON2_TYPE_ROOTPAGE,
+		},
+		.platform_info = {
+			.pci_mmconfig_base = 0x2000000,
+			.pci_mmconfig_end_bus = 0,
+			.pci_is_virtual = 1,
+			.arm = {
+				.gic_version = 2,
+				.gicd_base = 0x01c81000,
+				.gicc_base = 0x01c82000,
+				.gich_base = 0x01c84000,
+				.gicv_base = 0x01c86000,
+				.maintenance_irq = 25,
+			},
 		},
 		.root_cell = {
 			.name = "Banana-Pi",
 
 			.cpu_set_size = sizeof(config.cpus),
 			.num_memory_regions = ARRAY_SIZE(config.mem_regions),
-			.num_irqchips = 1,
+			.num_irqchips = ARRAY_SIZE(config.irqchips),
+			.num_pci_devices = ARRAY_SIZE(config.pci_devices),
+
+			.vpci_irq_base = 108,
 		},
 	},
 
@@ -156,23 +180,58 @@ struct {
 		/* RAM */ {
 			.phys_start = 0x40000000,
 			.virt_start = 0x40000000,
-			.size = 0x3c000000,
+			.size = 0x3bf00000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_EXECUTE,
 		},
-		/* Framebuffer */ {
-			.phys_start = 0x7f700000,
-			.virt_start = 0x7f700000,
-			.size = 9<<20,
-			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
-				JAILHOUSE_MEM_IO,
+		/* IVSHMEM shared memory region */ {
+			.phys_start = 0x7bf00000,
+			.virt_start = 0x7bf00000,
+			.size = 0x100000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE,
 		},
-	},
-	.irqchips = {
-		/* GIC */ {
-			.address = 0x2f000000,
-			.pin_bitmap = 0xffffffffffffffff,
+		/* TWI, PS2-0,PS2-1 memory region */ {
+			.phys_start = 0x01c2a000,
+			.virt_start = 0x01c2a000,
+			.size = 4<<10,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO | JAILHOUSE_MEM_IO_32,
+		},
+		/* TP memory region */ {
+			.phys_start = 0x01c25000,
+			.virt_start = 0x01c25000,
+			.size = 0x30,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO | JAILHOUSE_MEM_IO_32,
+		},
+		/* Framebuffer */ {
+			.phys_start = 0x7f736000,
+			.virt_start = 0x7f736000,
+			.size = 9<<20,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE | JAILHOUSE_MEM_IO,
 		},
 	},
 
+	.irqchips = {
+		/* GIC */ {
+			.address = 0x01c81000,
+			.pin_base = 32,
+			.pin_bitmap = {
+				0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff
+			},
+		},
+	},
+
+	.pci_devices = {
+		{
+			.type = JAILHOUSE_PCI_TYPE_IVSHMEM,
+			.bdf = 0x00,
+			.bar_mask = {
+				0xffffff00, 0xffffffff, 0x00000000,
+				0x00000000, 0x00000000, 0x00000000,
+			},
+			.shmem_region = 16,
+			.shmem_protocol = JAILHOUSE_SHMEM_PROTO_VETH,
+		},
+	},
 };
