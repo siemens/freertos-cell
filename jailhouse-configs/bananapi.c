f6a84c1deb41b015e5884b0bbd8ba46bda22e906
Dr. Johann Pfefferl <johann.pfefferl@siemens.com>
2019-05-15 14:02:10 +0200

    Fix compiler warning
    Signed-off-by: Dr. Johann Pfefferl <johann.pfefferl@siemens.com>

--- jailhouse-configs/bananapi.c
+++ jailhouse-configs/bananapi.c
@@ -15,7 +15,7 @@
 #include <jailhouse/types.h>
 #include <jailhouse/cell-config.h>
 
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#define __ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
 struct {
 	struct jailhouse_system header;
@@ -59,9 +59,9 @@ struct {
 			.name = "Banana-Pi",
 
 			.cpu_set_size = sizeof(config.cpus),
-			.num_memory_regions = ARRAY_SIZE(config.mem_regions),
-			.num_irqchips = ARRAY_SIZE(config.irqchips),
-			.num_pci_devices = ARRAY_SIZE(config.pci_devices),
+			.num_memory_regions = __ARRAY_SIZE(config.mem_regions),
+			.num_irqchips = __ARRAY_SIZE(config.irqchips),
+			.num_pci_devices = __ARRAY_SIZE(config.pci_devices),
 
 			.vpci_irq_base = 108,
 		},
@@ -302,7 +302,7 @@ struct {
 				0x00000000, 0x00000000, 0x00000000,
 			},
 			/* Take the last entry of the mem_regions array */
-			.shmem_region = ARRAY_SIZE(config.mem_regions) - 1,
+			.shmem_region = __ARRAY_SIZE(config.mem_regions) - 1,
 			.shmem_protocol = JAILHOUSE_SHMEM_PROTO_VETH,
 		},
 	},
