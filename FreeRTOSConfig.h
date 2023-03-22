ac8ca4739e0d3d39054dec6b2246f1bb3a5718ee
Dr. Johann Pfefferl <johann.pfefferl@siemens.com>
2023-03-22 08:45:35 +0100

    freertos: upgrade to version FreeRTOSv202212.01

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -135,7 +135,7 @@ to exclude the API function. */
 format the raw data provided by the uxTaskGetSystemState() function in to human
 readable ASCII form.  See the notes in the implementation of vTaskList() within
 FreeRTOS/Source/tasks.c for limitations. */
-#define configUSE_STATS_FORMATTING_FUNCTIONS	1
+#define configUSE_STATS_FORMATTING_FUNCTIONS	0
 
 /* The size of the global output buffer that is available for use when there
 are multiple command interpreters running at once (for example, one on a UART
